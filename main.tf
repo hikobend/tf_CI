0c50c59926b4699ac6442877381644a7bff5b96a
hikobend <hiko.bkend@gmail.com>
2023-04-29 17:16:35 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "1.3.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "vpc"
+  }
+}
